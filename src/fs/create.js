3feef8ad67c169bab45bec074318950bfcf725d7
MirzaabdullayevDev <mirzaabdullayev023@gmail.com>
2022-06-02 17:44:16 +0500

    feat: add fs create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,21 @@
+import fs from 'fs'
+import path from 'path'
+
 export const create = async () => {
-    // Write your code here 
+    return new Promise((resolve, reject) => {
+        fs.stat(
+            path.join(path.dirname('create.js'), 'files', 'fresh.txt'),
+            (err) => {
+                if (!err) throw new Error('FS operation failed')
+            })
+
+        fs.writeFile(
+            path.join(path.dirname('create.js'), 'files', 'fresh.txt'), 'I am fresh and young',
+            (err) => {
+                // err ? reject(err) : resolve
+                if (err) reject(err);
+                else resolve()
+
+            })
+    })
 };
