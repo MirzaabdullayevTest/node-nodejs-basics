79be131ae3d57a56f347b1fb39cbc5c159ea6dca
MirzaabdullayevDev <mirzaabdullayev023@gmail.com>
2022-06-03 16:10:56 +0500

    feat: add fs copy.js and update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,18 @@
 import fs from 'fs'
 import path from 'path'
 
+const dir = path.dirname('create.js')
+
 export const create = async () => {
-    return new Promise((resolve, reject) => {
-        fs.stat(
-            path.join(path.dirname('create.js'), 'files', 'fresh.txt'),
-            (err) => {
-                if (!err) throw new Error('FS operation failed')
-            })
+    await fs.stat(
+        path.join(dir, 'files', 'fresh.txt'),
+        (err) => {
+            if (!err) throw new Error('FS operation failed')
+        })
 
-        fs.writeFile(
-            path.join(path.dirname('create.js'), 'files', 'fresh.txt'), 'I am fresh and young',
-            (err) => {
-                // err ? reject(err) : resolve
-                if (err) reject(err);
-                else resolve()
-
-            })
-    })
+    await fs.writeFile(
+        path.join(dir, 'files', 'fresh.txt'), 'I am fresh and young',
+        (err) => {
+            if (err) console.error(err);
+        })
 };
